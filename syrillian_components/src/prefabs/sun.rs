5c9d2eeba50703194df755eab5abb56f159fbc80
willow <git@willow.moe>
2026-02-02 15:04:49 +0100

    fix: sunlight angle

--- syrillian_components/src/prefabs/sun.rs
+++ syrillian_components/src/prefabs/sun.rs
@@ -13,7 +13,7 @@ impl Prefab for SunPrefab {
     fn build(&self, world: &mut World) -> GameObjectId {
         let mut obj = world.new_object(self.prefab_name());
         obj.transform.set_position(-20, 20, -20);
-        obj.transform.set_euler_rotation_deg(45, 0, 45);
+        obj.transform.set_euler_rotation_deg(-45, 45, 45);
 
         obj.add_component::<SunLightComponent>();
 
